3a5f0823095afe9186082dc92bb566f2e7e8ebac
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2021-06-03 21:27:41 +0300

    Update task_from_interview.py

--- task_from_interview.py
+++ task_from_interview.py
@@ -18,4 +18,27 @@ factorial = reduce(lambda x,y:x*y, numbers_list)
 print(factorial)
 
 
+# другая компания
+# Нужно написать функцию которя принимает два списка и выдает список с поочередными элементами из списка 1 и 2
+# [1, 2, 3, 4], [7,6,9] =>> [1, 7, 2, 6, 3, 9, 4]
+def foo(list_a, list_b):
+    index_number = max(len(list_a), len(list_b))
+    new_list = []
+    for i in range(index_number):
+        try:
+            new_list.append(list_a[i])
+        except:
+            pass
+        try:
+            new_list.append(list_b[i])
+        except:
+            pass
+
+    return new_list
+
+
+list_a = [1, 2]
+list_b = [7, 6, 9]
+
+print(foo(list_a, list_b))
 
